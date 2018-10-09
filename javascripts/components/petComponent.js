b617bce463343d392faa6a18d22fcc2a61784fda
Timothy Harley <timothy.d.harley@gmail.com>
2018-10-09 18:25:48 -0500

    type colors added

--- javascripts/components/petComponent.js
+++ javascripts/components/petComponent.js
@@ -5,6 +5,22 @@ const printToDom = (stringToPrint, divId) => {
     selectedDiv.innerHTML = stringToPrint;
 };
 
+
+const typeColor = () => {
+    const footer = document.getElementsByClassName('footer');
+    for (let i=0; i < footer.length; i++){
+        const element = footer[i];
+        if(element.innerHTML === 'dog'){
+            element.classList.add('bg-primary')
+        } else if (element.innerHTML === 'cat'){
+            element.classList.add('bg-danger') 
+        } else if (element.innerHTML === 'dino'){
+            element.classList.add('bg-success')
+        };
+    }
+};
+
+
 const showAnimals = (petArray) => {
     let domString = ''
     petArray.forEach((pet) => {
@@ -15,21 +31,11 @@ const showAnimals = (petArray) => {
         domString +=        `<h5 class="card-title">${pet.color}</h5>`
         domString +=        `<p class="card-text">${pet.specialSkill}</p>`
         domString +=    `</div>`
-        domString +=    `<div class="card-footer text-muted footer">${pet.type}</div>`
+        domString +=    `<div class="card-footer text-light footer">${pet.type}</div>`
         domString += `</div>`
     })
     printToDom(domString,'printDiv');
-};
-
-const typeColor = () => {
-    const footer = document.getElementsByClassName('footer');
-    for (let i=0; i < footer.length; i++){
-        const element = footer[i];
-        if(element.innerHTML === 'dog'){
-            element.parentNode.classList.add('bg-primary')
-        } else {element.parentNode.classList.add('bg-warning') 
-        }
-    }
+    typeColor();
 };
 
 export {showAnimals, typeColor}
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,9 @@
 import { getAnimals } from './data/petData.js';
 import { sortEvents } from './events.js';
-import { typeColor } from './components/petComponent.js';
 
 const initApp = () => {
     getAnimals();
     sortEvents();
-    typeColor();
 };
 
 initApp();
